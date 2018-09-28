e8e6af4aab690dac0c8b8d6161b3f323a826c3d6
ZairaRiv <zairariv@mail.fresnostate.edu>
2018-09-28 16:33:10 -0700

    If you blow it...

--- Part1/calc.js
+++ Part1/calc.js
@@ -1,48 +1,63 @@
-var numbers = [];
-var pos=0;
 var ops = [];
 
-function captureKey(key){
-	if (numbers[pos] === null){
-		numbers[pos];
-	}
-	numbers[pos]=numbers[pos]+key;
-	console.log(numbers[pos]);
-
-
+function captureKey(key) {
+  ops.push(key);
+  console.log(key);
 }
 
-
-
-function incArray(){
-	pos++;
+function equals() {
+  let currentNumber = null;
+  let ans = 0;
+  for (let i = 0; i < ops.length; i++) {
+    //to determine what to do with it
+    switch (ops) {
+      case "+":
+        if (ans == 0) {
+          ans = currentNumber;
+        }
+        addKey;
+      case "-":
+        subKey;
+      case "*":
+        multKey;
+      case "/":
+        divKey;
+      default:
+        if (currentNumber === null) {
+          currentNumber = ops[i];
+        } else {
+          currentNumber = currentNumber * 10 + ops[i];
+        }
+    }
+  } //end for
 }
 
-function addKey(a,b){
-     return a + b;     
+function incArray() {
+  pos++;
 }
 
-function subKey(a,b){
-    return a - b; 
+function addKey(a, b) {
+  return a + b;
 }
 
-function multKey(a,b){
-    return a * b; 
+function subKey(a, b) {
+  return a - b;
 }
 
-function divKey (a,b){
-    return a/b; 
+function multKey(a, b) {
+  return a * b;
 }
 
-function squareKey(a,b){
-    return Math.pow(a,b);
+function divKey(a, b) {
+  return a / b;
 }
 
-function sqrRt (b){
-    return Math.sqrt(b);
+function squareKey(a, b) {
+  return Math.pow(a, b);
 }
 
-function clear (a,b){
-
+function sqrRt(b) {
+  return Math.sqrt(b);
 }
 
+function clear(a, b) {}
