fcfd0145832b9ba851907cf350b45e68cc072d09
ZairaRiv <zairariv@mail.fresnostate.edu>
2018-09-28 15:47:54 -0700

    Peasant JS

--- Part1/calc.js
+++ Part1/calc.js
@@ -1,1 +1,32 @@
+function captureKey(key){
+    
+}
+
+function addKey(a,b){
+     return a + b;     
+}
+
+function subKey(a,b){
+    return a - b; 
+}
+
+function multKey(a,b){
+    return a * b; 
+}
+
+function divKey (a,b){
+    return a/b; 
+}
+
+function squareKey(a,b){
+    return Math.pow(a,b);
+}
+
+function sqrRt (b){
+    return Math.sqrt(b);
+}
+
+function clear (a,b){
+
+}
 
