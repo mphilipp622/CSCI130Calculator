ff519f5800e1df5592c4694c1218cb4cffa6e421
Joseph Sesate <sesate09@gmail.com>
2018-09-28 16:39:18 -0700

    Updated Functionality
    -SD is not working as intended.

--- Part2/part2.js
+++ Part2/part2.js
@@ -1,133 +1,58 @@
-//
-//
-//
 
-
-        function makeArray(n)
+function minValue(vectorArray)
+{
+    var smallestValue = vectorArray[0];
+    for(i = 0; i <  vectorArray.length; i++)
+    {
+        if(smallestValue > vectorArray[i])
         {
-            var vectorArray = new Array(n)
-            return vectorArray;
+            smallestValue = vectorArray[i];
         }
-    
+      
+    }
+    return smallestValue;
+}  
 
-        function minValue(vectorArray)
+
+
+function maxValue(vectorArray)
+{
+    var maxValue = vectorArray[0];
+    for(i = 0; i < vectorArray.length; i++)
+    {
+        if(maxValue < vectorArray[i])
         {
-            var smallestValue = vectorArray[0];
-            for(i = 0; i < vectorArray.length(); i++)
-            {
-                if(smallestValue > vectorArray[i])
-                {
-                    smallestValue = vectorArray[i];
-                }
-                return smallestValue;
-            }
+            maxValue = vectorArray[i];
         }
+    }
+    return maxValue;
+}
 
 
-        function maxValue(vectorArray)
+function meanValue(vectorArray)
         {
-            var maxValue = vectorArray[0];
-            for(i = 0; i < vectorArray.length(); i++)
-            {
-                if(maxValue < vectorArray[i])
-                {
-                    maxValue = vectorArray[i];
-                }
-            }
-            return maxValue;
-        }
+    var total = 0;
+    for(i=0; i < vectorArray.length; i++)
+    {
+        total = total + vectorArray[i];
+    }
+    return total/vectorArray.length;
+}
 
+function standardDeviation(vectorArray)
+{
+    var mValue = meanValue(vectorArray);
 
-        function meanValue(vectorArray)
-        {
-            var total = 0;
-            for(i=0; i < vectorArray.length(); i++)
-            {
-                total = total + vectorArray[i];
-            }
-            return total/vectorArray.length();
-        }
-
-        function standardDeviation(vectorArray)
-        {
-            var meanValue = meanValue(vectorArray);
-
-            //next sub mean from all nums in vect
-            for(i = 0; i < arrayVector.length(); i++)
-            {
-                arrayVector[i] = arrayVector[i]-meanValue;
-                arrayVector[i] = arrayVector[i]*arrayVector[i];
-            }
+    //next sub mean from all nums in vect
+    for(i = 0; i < vectorArray.length; i++)
+    {
+        vectorArray[i] = vectorArray[i] - vectorArray;
+        vectorArray[i] = vectorArray[i] * vectorArray[i];
+    }
             
-           var variance = meanValue(arrayVector);
+    var variance = meanValue(vectorArray);
+    var temp = Math.sqrt(variance);
+    return temp;
 
-           return Math.sqrt(variance);
+}
 
-
-           Math.standardDeviation(vectorArray);
-
-        }
-        function makeArray(n)
-        {
-            var vectorArray = new Array(n)
-            return vectorArray;
-        }
-    
-
-        function minValue(vectorArray)
-        {
-            var smallestValue = vectorArray[0];
-            for(i = 0; i < vectorArray.length(); i++)
-            {
-                if(smallestValue > vectorArray[i])
-                {
-                    smallestValue = vectorArray[i];
-                }
-                return smallestValue;
-            }
-        }
-
-
-        function maxValue(vectorArray)
-        {
-            var maxValue = vectorArray[0];
-            for(i = 0; i < vectorArray.length(); i++)
-            {
-                if(maxValue < vectorArray[i])
-                {
-                    maxValue = vectorArray[i];
-                }
-            }
-            return maxValue;
-        }
-
-
-        function meanValue(vectorArray)
-        {
-            var total = 0;
-            for(i=0; i < vectorArray.length(); i++)
-            {
-                total = total + vectorArray[i];
-            }
-            return total/vectorArray.length();
-        }
-
-        function standardDeviation(vectorArray)
-        {
-            var meanValue = meanValue(vectorArray);
-
-            //next sub mean from all nums in vect
-            for(i = 0; i < arrayVector.length(); i++)
-            {
-                arrayVector[i] = arrayVector[i]-meanValue;
-                arrayVector[i] = arrayVector[i]*arrayVector[i];
-            }
-            
-           var variance = meanValue(arrayVector);
-
-           return Math.sqrt(variance);
-
-
-           Math.standardDeviation(vectorArray);
-
-        }
