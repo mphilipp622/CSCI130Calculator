e7e7bb6042fa6646c5f96fff10ec9779efbb1fd6
unknown <garret95@mail.fresnostate.edu>
2018-09-28 16:13:54 -0700

    + - * functions

--- Part3/Part3.js
+++ Part3/Part3.js
@@ -22,6 +22,10 @@ function getDimensions(input) {
 
 }
 
+const matrixA = math.add(matrixA, matrixB);
+const matrixS = math.subtract(matrixA, matrixB);
+const matrixM = math.multiply(matrixA, matrixB);
+
 function transposeArray(array){
     var h = a[0] instanceof Array ? a[0].length : 0;// height of array
     var w = a.length || 0;                          // width of array
