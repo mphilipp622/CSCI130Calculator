5f7182bbbf751af9842d483e0adcf28ffcc1d8b7
unknown <garret95@mail.fresnostate.edu>
2018-09-28 15:44:29 -0700

    added create matrices

--- Part3/Part3.js
+++ Part3/Part3.js
@@ -3,4 +3,20 @@ function getDimensions(input) {
     var n2 = document.getElementById('n2').value;
     var m1 = document.getElementById('m1').value;
     var m2 = document.getElementById('n2').value;
+
+    var matrixA = [];
+    for (var i = 0; i < n1; i++) {
+        matrixA[i] = [];
+        for (var j = 0; j < m1; j++) {
+            matrixA[i][j] = undefined;
+        }
+    }
+
+    var matrixB = [];
+    for (var i = 0; i < n2; i++) {
+        matrixA[i] = [];
+        for (var j = 0; j < m2; j++) {
+            matrixA[i][j] = undefined;
+        }
+    }
 }
