a24b4546613307bd268638f3b3677679bf2d295f
unknown <garret95@mail.fresnostate.edu>
2018-09-28 15:40:09 -0700

    push

--- Part3/Part3.js
+++ Part3/Part3.js
@@ -1,6 +1,6 @@
 function getDimensions(input) {
-    var n1 = document.getElementById('n1');
-    var n2 = document.getElementById('n2');
-    var m1 = document.getElementById('m1');
-    var m2 = document.getElementById('n2');
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    var m1 = document.getElementById('m1').value;
+    var m2 = document.getElementById('n2').value;
 }
